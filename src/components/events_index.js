aa5f1c6a19e33c46efc17f0466641dd940f22fd4
Hayato Shigematsu <shigema2-h@pjlincoln.onmicrosoft.com>
2020-10-16 17:43:41 +0900

    implement events index

---
+++ src/components/events_index.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux'
+import _ from 'lodash'
+import { readEvents } from '../actions'
+// class App extends Component {
+//   render(){
+//     const greeting = "Hi,Tom!";
+//     const dom = <h1 className = "foo">{greeting}</h1>
+//     return (
+//       <React.Fragment>
+//         <label  htmlFor = "bar">bar</label>
+//         <input type = "text" onChange ={() => {console.log("I am clicked!")}} />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+class EventsIndex extends Component{
+  componentDidMount(){
+    this.props.readEvents()
+  }
+
+  renderEvents(){
+    return _.map(this.props.events, event => (
+      <tr key = {event.id}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+      </tr>
+    ))
+  }
+
+  render(){
+
+    return(
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderEvents()}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+const mapStateToProps = state => ({events:state.events})
+const mapDispatchToProps = ({
+  readEvents
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
